dd1cc80ab0dc0da372d2bd507e45a1b1ce48884d
Matthieu Rault <matthieu.rault@oclock.school>
2024-06-05 14:24:12 +0200

    add error 500, modif controller, and homepage

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,6 +1,15 @@
+const dataMapper = require('../dataMapper');
+
 const mainController = {
-  homePage(req, res) {
-    res.render("main/home");
+  homePage: async (req, res) => {
+    try {
+      // Récupérer tout les cafés de la BDD
+      const coffees = await dataMapper.findAllCoffees();
+      res.render("main/home", { coffees });
+    } catch (error) {
+      console.error(error);
+      res.status(500).render('500');
+    }
   },
 
   catalogPage(req, res) {
--- app/dataMapper.js
+++ app/dataMapper.js
@@ -1,2 +1,12 @@
 const client = require('./database.js');
 
+const dataMapper = {
+  findAllCoffees: async () => {
+    const result = await client.query(`SELECT * FROM "coffee" LIMIT 3`);
+    const coffees = result.rows;
+    return coffees;
+  }
+}
+
+module.exports = dataMapper;
+
