b114ce2819f7c46be7cdfeed5f9396c6a6fc9ebf
Matthieu Rault <matthieu.rault@oclock.school>
2024-06-05 17:14:55 +0200

    modif css, view detail, view catalog, le maincontroller et le datamapper

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -3,21 +3,34 @@ const dataMapper = require('../dataMapper');
 const mainController = {
   homePage: async (req, res) => {
     try {
-      // Récupérer tout les cafés de la BDD
-      const coffees = await dataMapper.findAllCoffees();
+      // Récupérer tout les cafés de la BDD avec une limite de 3 cafés
+      const coffees = await dataMapper.findCoffeesLimit();
       res.render("main/home", { coffees });
     } catch (error) {
       console.error(error);
       res.status(500).render('500');
     }
   },
-
-  catalogPage(req, res) {
-    res.render("main/catalog");
+  catalogPage: async (req, res) => {
+    try {
+      // Récupérer tout les cafés de la BDD avec une limite de 3 cafés de façon aléatoire
+      const randomCoffees = await dataMapper.findCoffeesLimitRandom();
+      res.render("main/catalog", { randomCoffees });
+    } catch (error) {
+      console.error(error);
+      res.status(500).render('500');
+    }
   },
-
-  coffeeDetailsPage(req, res) {
-    res.render("main/detail");
+  coffeeDetailsPage: async (req, res) => {
+    try {
+      // Récupérer l'ID de l'article
+      const articleId = parseInt(req.params.id);
+      const coffee = await dataMapper.findDetailCoffee(articleId);      
+      res.render("main/detail", { coffee });
+    } catch (error) {
+      console.error(error);
+      res.status(500).render('500');
+    }
   }
 };
 
--- app/dataMapper.js
+++ app/dataMapper.js
@@ -1,10 +1,25 @@
 const client = require('./database.js');
 
 const dataMapper = {
-  findAllCoffees: async () => {
+  findCoffeesLimit: async () => {
     const result = await client.query(`SELECT * FROM "coffee" LIMIT 3`);
     const coffees = result.rows;
     return coffees;
+  },
+  findCoffeesLimitRandom: async () => {
+    const result = await client.query(`SELECT * FROM "coffee" ORDER BY RANDOM() LIMIT 3`);
+    const coffees = result.rows;
+    return coffees
+  },
+  findAllCoffees: async () => {
+    const result = await client.query(`SELECT * FROM "coffee"`);
+    const coffees = result.rows;
+    return coffees;
+  },
+  findDetailCoffee: async (id) => {
+    const result = await client.query(`SELECT * FROM "coffee" WHERE id = $1`, [id]);
+    const coffee = result.rows[0];
+    return coffee;
   }
 }
 
