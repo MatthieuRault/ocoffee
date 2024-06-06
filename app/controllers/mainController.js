89df52730d56a80adccca8dfd045806bc81a7ca3
Matthieu Rault <matthieu.rault@oclock.school>
2024-06-06 12:07:12 +0200

    modif css, requete SQL du dataMapper pour le detail d'un produit et inversement req sql de la homepage et catalog

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -3,9 +3,9 @@ const dataMapper = require('../dataMapper');
 const mainController = {
   homePage: async (req, res) => {
     try {
-      // Récupérer tout les cafés de la BDD avec une limite de 3 cafés
-      const coffees = await dataMapper.findCoffeesLimit();
-      res.render("main/home", { coffees });
+      // Récupérer tout les cafés de la BDD avec une limite de 3 cafés de façon aléatoire
+      const randomCoffees = await dataMapper.findCoffeesLimitRandom();
+      res.render("main/home", { randomCoffees });      
     } catch (error) {
       console.error(error);
       res.status(500).render('error/500');
@@ -13,9 +13,9 @@ const mainController = {
   },
   catalogPage: async (req, res) => {
     try {
-      // Récupérer tout les cafés de la BDD avec une limite de 3 cafés de façon aléatoire
-      const randomCoffees = await dataMapper.findCoffeesLimitRandom();
-      res.render("main/catalog", { randomCoffees });
+      // Récupérer tout les cafés de la BDD avec une limite de 3 cafés
+      const coffees = await dataMapper.findCoffeesLimit();
+      res.render("main/catalog", { coffees });
     } catch (error) {
       console.error(error);
       res.status(500).render('error/500');
--- app/dataMapper.js
+++ app/dataMapper.js
@@ -15,13 +15,20 @@ const dataMapper = {
     const result = await client.query(`SELECT * FROM "coffee"`);
     const coffees = result.rows;
     return coffees;
-  },
+  },  
   findDetailCoffee: async (id) => {
-    const result = await client.query(`SELECT * FROM "coffee" WHERE id = $1`, [id]);
+    const result = await client.query(`SELECT coffee.*, category.name AS cat_name, category.id AS cat_id FROM "coffee" JOIN 
+    "category" ON coffee.category_id = category.id WHERE coffee.id = $1`, [id]);
     const coffee = result.rows[0];
+    coffee.available = coffee.available ? 'Oui' : 'Non';
+    // Equivalent de :
+    //   if (coffee.available) {
+    //   coffee.available = "Oui";
+    // } else {
+    //   coffee.available = "Non";
+    // }
     return coffee;
   }
 }
 
 module.exports = dataMapper;
-
