2876f3e57551d5dfa1fe53eaa49bceeb3848a26a
Matthieu Rault <rault.matthieu@hotmail.fr>
2024-10-12 20:50:31 +0200

    edit: supabase

--- app/database.js
+++ app/database.js
@@ -2,13 +2,7 @@
 const pg = require("pg");
 
 // 2. Créer un client
-const client = new pg.Client({
-  connectionString: process.env.PG_URL,
-  ssl: {
-    rejectUnauthorized: false,
-    sslmode: "require",
-  },
-});
+const client = new pg.Client(process.env.PG_URL);
 
 // 3. Connecter le client
 client.connect();
