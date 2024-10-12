88214225d6be67994059073f1d644656ba2778cf
Matthieu Rault <rault.matthieu@hotmail.fr>
2024-10-12 12:52:21 +0200

    update: add validations

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,14 +1,14 @@
-const dataMapper = require('../dataMapper');
+const dataMapper = require("../dataMapper");
 
 const mainController = {
   homePage: async (req, res) => {
     try {
       // Récupérer tout les cafés de la BDD avec une limite de 3 cafés de façon aléatoire
       const randomCoffees = await dataMapper.findCoffeesLimitRandom();
-      res.render("main/home", { randomCoffees });      
+      res.render("main/home", { randomCoffees });
     } catch (error) {
       console.error(error);
-      res.status(500).render('error/500');
+      res.status(500).render("error/500");
     }
   },
   catalogPage: async (req, res) => {
@@ -18,36 +18,50 @@ const mainController = {
       res.render("main/catalog", { coffees });
     } catch (error) {
       console.error(error);
-      res.status(500).render('error/500');
+      res.status(500).render("error/500");
     }
   },
   coffeeDetailsPage: async (req, res) => {
     try {
       // Récupérer l'ID de l'article
       const articleId = parseInt(req.params.id);
-      const coffee = await dataMapper.findDetailCoffee(articleId);      
+      // Si l'ID n'est pas un nombre, on renvoie une erreur 400
+      if (isNaN(articleId)) {
+        return res
+          .status(400)
+          .render("error/400", { message: "Mauvaise requête" });
+      }
+      const coffee = await dataMapper.findDetailCoffee(articleId);
+      // Si le café n'existe pas, on renvoie une erreur 404
+      if (!coffee) {
+        return res.status(404).render("error/404");
+      }
       res.render("main/detail", { coffee });
     } catch (error) {
       console.error(error);
-      res.status(500).render('error/500');
+      res.status(500).render("error/500");
     }
   },
   aboutPage: (req, res) => {
     try {
-      res.render('main/about');
+      res.render("main/about");
     } catch (error) {
-      console.error(500).render('error/500');
+      console.error(500).render("error/500");
     }
   },
   listCoffees: async (req, res) => {
     try {
-      const coffees = await dataMapper.findAllCoffees();      
-      res.render('main/catalog', { coffees });
+      const coffees = await dataMapper.findAllCoffees();
+      // Si aucun café n'est trouvé, on renvoie une erreur 404
+      if (!coffees) {
+        return res.status(404).render("error/404");
+      }
+      res.render("main/catalog", { coffees });
     } catch (error) {
       console.error(error);
-      res.status(500).render('error/500');
+      res.status(500).render("error/500");
     }
-  }
+  },
 };
 
 module.exports = mainController;
